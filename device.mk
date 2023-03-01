89cbbe88f35cc6276b1acccdbb3c69264047f575
Luofan Chen <clfbbn@gmail.com>
2023-03-01 20:51:49 +0800

    sm8250-common: Build lineage health HAL
    Change-Id: I1ca5f25f3a2d8734647b9cc0ff4885b0b817c6ad
    Signed-off-by: althafvly <althafvly@gmail.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- device.mk
+++ device.mk
@@ -308,6 +308,14 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light-service.xiaomi
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm
+    
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
