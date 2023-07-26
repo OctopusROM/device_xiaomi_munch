b01bf786fd252701540cf665a5982ca7e3b175f1
itsurboimasarou <medicspam6@gmail.com>
2023-07-26 09:01:37 +0700

    munch: rro_overlays: Import device specific RROs
    Signed-off-by: itsurboimasarou <medicspam6@gmail.com>

--- device.mk
+++ device.mk
@@ -33,6 +33,17 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+PRODUCT_PACKAGES += \
+    SettingsOverlay22021211RC \
+    SettingsOverlay22021211RG \
+    SettingsOverlay22021211RI \
+    SettingsProviderOverlay22021211RC \
+    SettingsProviderOverlay22021211RG \
+    SettingsProviderOverlay22021211RI \
+    WifiOverlay22021211RC \
+    WifiOverlay22021211RG \
+    WifiOverlay22021211RI
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 31
 
