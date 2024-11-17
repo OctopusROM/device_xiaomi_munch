46ff3a041d6ffb1583f6f7c087c027931e3b7f87
LittleChest <81231195+LittleChest@users.noreply.github.com>
2024-11-17 13:07:20 +0000

    munch: Now say PixelOS!

--- aosp_munch.mk
+++ aosp_munch.mk
@@ -8,18 +8,19 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PixelOS stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from munch device
 $(call inherit-product, device/xiaomi/munch/device.mk)
 
-PRODUCT_NAME := lineage_munch
+PRODUCT_NAME := aosp_munch
 PRODUCT_DEVICE := munch
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO F4
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+TARGET_BOOT_ANIMATION_RES := 1080
 
 BUILD_FINGERPRINT := Redmi/munch/munch:13/RKQ1.211001.001/V816.0.4.0.ULMMIXM:user/release-keys
